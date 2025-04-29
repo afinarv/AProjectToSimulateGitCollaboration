16a0ca2cfa572a9eb71a4f08c3b14548e0f6a279
Afina R. Vinci <afinarv@gmail.com>
2025-04-29 23:25:14 +0700

    Initial Commit

---
+++ AProjectToSimulateGitCollaboration/AProjectToSimulateGitCollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AProjectToSimulateGitCollaborationApp.swift
+//  AProjectToSimulateGitCollaboration
+//
+//  Created by Afina R. Vinci on 29/04/25.
+//
+
+import SwiftUI
+
+@main
+struct AProjectToSimulateGitCollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
